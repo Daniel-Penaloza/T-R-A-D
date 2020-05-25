03a6ab59221477d6dbc65ee71a9755772badb15b
Daniel Penaloza <danielpenalozatj@gmail.com>
2020-05-25 13:46:03 -0700

    Numbers

---
+++ First Section/datatypes.dart
@@ -0,0 +1,28 @@
+main() {
+  
+  // Numbers
+  int a = 10;
+  double b = 10.5;
+  int c;
+
+  int x = 10, y = 20, z = 30;
+  print(x);
+  print(y);
+  print(z);
+
+  // Strings
+
+
+  // Booleans
+
+
+  // Lists
+
+
+
+  // Sets
+
+
+
+  // Maps
+}
