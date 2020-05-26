0883e117b1cd443f34b36ef6d85a10b5c2481dfd
Daniel Penaloza <danielpenalozatj@gmail.com>
2020-05-25 19:46:16 -0700

    Operators

---
+++ Second Section/operators.dart
@@ -0,0 +1,22 @@
+main() {
+  int add = 10 + 5;
+  int subs = 10 - 5;
+  double div = 10 / 2;
+  double mod = 10.0 % 3;
+  int by = 10 * 4;
+  int negation = -by; // Usado para cambiar el valor de la expresion
+  int divInt = 10 ~/ 3;
+  int increment = by++;
+  int decrement = by--;
+
+
+  print(add);
+  print(subs);
+  print(div);
+  print(mod);
+  print(by);
+  print(negation);
+  print(divInt);
+  print(increment);
+  print(decrement);
+}
