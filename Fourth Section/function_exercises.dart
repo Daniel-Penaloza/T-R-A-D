b504baa6b0a3c62113f8a1fbf4787e0e77e917bc
Daniel Penaloza <danielpenalozatj@gmail.com>
2020-05-27 18:00:45 -0700

    DART EXCERCISE

---
+++ Fourth Section/function_exercises.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+main() {
+
+  // Ejemplo: 
+  // Crear una función para imprimir STDOUTS en lugar de 
+  // la siguiente línea. Luego todos los stdouts deberían ser
+  // llamados usando nuestra función personalizada
+  
+    for(var i = 1; i <=2; i++){
+      if(i == 1){
+        printInformation(position: i, salary: 1500);
+      } else if (i == 2) {
+        printInformation(position: i, salary: 1800);
+      } else {
+        break;
+      };
+      
+    };
+}
+
+printInformation({int position, double salary}){
+  stdout.writeln('=========== Usuario ${position} =============');
+  stdout.writeln('¿Cúal es su nombre?');
+  String nombre = stdin.readLineSync();
+
+  stdout.writeln('¿Qué edad tienes?');
+  String edad = stdin.readLineSync();
+  
+  stdout.writeln('¿En qué país naciste?');
+  String pais = stdin.readLineSync();
+
+    final Map<String, dynamic> usuario = {
+    'nombre': nombre,
+    'edad'  : edad,
+    'pais'  : pais
+  };
+
+  stdout.writeln('Usuario ${position} sin deducciones');
+  stdout.writeln( usuario );
+  double deducciones = salary * 0.15;
+  double salarioNeto = salary - deducciones;
+
+  usuario['salario']     = salary;
+  usuario['deducciones'] = deducciones;
+  usuario['salarioNeto'] = salarioNeto;
+
+  stdout.writeln(usuario);
+}
