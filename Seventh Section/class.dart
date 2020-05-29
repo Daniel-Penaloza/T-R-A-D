9850ae0edc5334002c115167dea27b8bc75fe51b
Daniel Penaloza <danielpenalozatj@gmail.com>
2020-05-28 19:29:19 -0700

    Class accross multiple files

--- Seventh Section/class.dart
+++ Seventh Section/class.dart
@@ -1,25 +1,10 @@
+import 'class/person.dart';
+
 main(){
   
   final person = new Person();
   person.name = 'Dick';
   person.age = 23;
   person.bio = 'This is the bio';
-
-  print(person.toString());
-}
-
-
-class Person {
-  // Fields and properties
-  String name;
-  int age;
-  String bio;
-
-  // Gets and sets
-
-  // Constructors
-
-  // Methods
-  @override
-  String toString() => '$name $age $bio';
+  print(person);
 }
---
+++ Seventh Section/class/person.dart
@@ -0,0 +1,14 @@
+class Person {
+  // Fields and properties
+  String name;
+  int age;
+  String bio;
+
+  // Gets and sets
+
+  // Constructors
+
+  // Methods
+  @override
+  String toString() => '$name $age $bio';
+}
