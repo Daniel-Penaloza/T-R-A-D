cdea0d5c2829093f9d983c6a955d9c966a3887af
Daniel Penaloza <danielpenalozatj@gmail.com>
2020-05-28 18:34:12 -0700

    Stream Controller Events

--- Sixth Section/stream.dart
+++ Sixth Section/stream.dart
@@ -3,12 +3,22 @@ import 'dart:async';
 main() {
   final streamController = StreamController();
 
-  streamController.stream.listen((data) { 
-    print('Flying! $data');
-  });
+  streamController.stream.listen(
+    (data) => print('Flying! $data'),
+    onError: (err) => print('Error! $err'),
+    onDone: () => print('Mision Completa'),
+    cancelOnError: false
+  );
 
   // Adding information to the stream
-  streamController.sink.add('Space X');
+  streamController.sink.add('Space X1');
+  streamController.sink.add('Space X2');
+  streamController.sink.add('Space X3');
+  streamController.sink.addError('Houston we have a problem');
+  streamController.sink.add('Space X5');
+  streamController.sink.add('Space X6');
+
+  streamController.sink.close();
 
   print('End of the main');
 }
