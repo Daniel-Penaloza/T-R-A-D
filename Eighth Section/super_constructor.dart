1407c0fda8c0a4a5447d179984503e58519a7ff0
Daniel Penaloza <danielpenalozatj@gmail.com>
2020-06-01 20:35:56 -0700

    Super Constructor

---
+++ Eighth Section/super_constructor.dart
@@ -0,0 +1,27 @@
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, this.age);
+
+  void printPerson() => print('Name: $name, Age: $age');
+}
+
+class Client extends Person {
+  String direction;
+  List<String> orders = [];
+
+  Client(int actualAge, String actualName, this.direction, this.orders):
+    super(actualName, actualAge);
+
+  get actualName => this.name;
+
+  void printOrder(){
+    print('Client information: \n Name: --- ${actualName} \n Age: --- ${age} \n Direction: ${direction} \n ${orders}');
+  }
+}
+
+main() { 
+  final client = new Client(20, 'Daniel', 'C Street', ['Order 1', 'Order 2']);
+  client.printOrder();
+}
