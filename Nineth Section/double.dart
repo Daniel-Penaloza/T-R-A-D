94cc98765e01a4c64ba60360496a9009c8abe312
Daniel Penaloza <danielpenalozatj@gmail.com>
2020-06-02 20:09:38 -0700

    Double Continuation

--- Nineth Section/double.dart
+++ Nineth Section/double.dart
@@ -1,4 +1,13 @@
 main() {
   double number = 3.1416;
+  double infinite = double.infinity;
+
   print('Signature: ${number.sign}');
+
+  print('isFinite: ${number.isFinite}');
+  print('isFinite: ${infinite.isFinite}');
+  print('abs: ${number.abs() }');
+  print('ceil: ${number.ceil()}');
+  print('ceilToDouble: ${number.ceilToDouble()}');
+  print('numberToRound: ${number.round()}');
 }
