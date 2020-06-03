721000aec624b83b0ee69adf9ccbf255866497d6
Daniel Penaloza <danielpenalozatj@gmail.com>
2020-06-02 21:25:57 -0700

    Adding http package

--- Tenth Section/packages/bin/packages.dart
+++ Tenth Section/packages/bin/packages.dart
@@ -1,5 +1,14 @@
-import 'package:packages/packages.dart' as packages;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) {
-  print('Hello world: ${packages.calculate()}!');
+  final url = 'https://reqres.in/api/users?page=2';
+  http.get(url).then((res){
+    final body = json.decode(res.body);
+    print(body);
+    print('page: ${body['page']}');
+    print('per_page ${body['per_page']}');
+    print('3rd Element ${body['data'][2]['id']}');
+  });
+
 }
