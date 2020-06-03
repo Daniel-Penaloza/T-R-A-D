da01787b5e946cf1bf869245dfd713f872871943
Daniel Penaloza <danielpenalozatj@gmail.com>
2020-06-02 20:25:22 -0700

    String complete

--- Nineth Section/string.dart
+++ Nineth Section/string.dart
@@ -7,4 +7,11 @@ main(){
   print('Length: ${fullName.length}');
   print('Contains: ${fullName.contains('D', 1)}');
   print('Ends: ${fullName.endsWith('a')}');
+
+  print('Operador []: ${name[2]}');
+  print('Operador *: ${name * 2}');
+  print('ReplaceAll: ${name.replaceAll('e', 'T')}');
+  print('Substring: ${name.substring(3)}');
+  print('IndexOf: ${name.indexOf('ie')}');
+  print('Split: ${name.split('i')}');
 }
