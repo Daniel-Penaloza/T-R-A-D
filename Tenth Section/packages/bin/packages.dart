d82d6f263609d6b6932956ed834bb6c24c3d4100
Daniel Penaloza <danielpenalozatj@gmail.com>
2020-06-03 15:52:53 -0700

    Adding a class to the response

--- Tenth Section/packages/bin/packages.dart
+++ Tenth Section/packages/bin/packages.dart
@@ -5,10 +5,5 @@ void main(List<String> arguments) {
   final url = 'https://reqres.in/api/users?page=2';
   http.get(url).then((res){
     final body = json.decode(res.body);
-    print(body);
-    print('page: ${body['page']}');
-    print('per_page ${body['per_page']}');
-    print('3rd Element ${body['data'][2]['id']}');
   });
-
 }
---
+++ Tenth Section/packages/lib/classes/request_response.dart
@@ -0,0 +1,29 @@
+// To parse this JSON data, do
+//
+//     final requestResponse = requestResponseFromJson(jsonString);
+
+import 'dart:convert';
+
+RequestResponse requestResponseFromJson(String str) => RequestResponse.fromJson(json.decode(str));
+
+String requestResponseToJson(RequestResponse data) => json.encode(data.toJson());
+
+class RequestResponse {
+    RequestResponse({
+        this.greeting,
+        this.instructions,
+    });
+
+    String greeting;
+    List<String> instructions;
+
+    factory RequestResponse.fromJson(Map<String, dynamic> json) => RequestResponse(
+        greeting: json["greeting"],
+        instructions: List<String>.from(json["instructions"].map((x) => x)),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "greeting": greeting,
+        "instructions": List<dynamic>.from(instructions.map((x) => x)),
+    };
+}
